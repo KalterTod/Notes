413d43c71addad0074e525e4039c84d598ca6bb0
Stephen Clark <stephenclark@MOBImage.local>
2014-04-03 11:33:31 -0400

    Added first test case to test.js

--- test/test.js
+++ test/test.js
@@ -15,7 +15,7 @@ describe('Notes Test', function() {
 				.post('/users') // Request type
 				// Body of the request for the request type listed in the line above
 				.send({ 
-					user_name: 'Test Person'
+					user_name: 'Test.Person'
 				})
 				// Tells function to end the request after a response is made. Otherwise, timeout. 
 				.end(function() {
@@ -24,7 +24,41 @@ describe('Notes Test', function() {
 	});
 
 	describe('Note Created', function() {
+		
+		/*  
+		*** This before function is nested inside this specific 'describe'
+		*** Therefore, anything in this block will execute before the 'it'
+		*** Can think of the 'it' as being the actual test case
+		*/
 
+		before(function(done) {
+			dest
+			// Similar to above, but now we are going to post to /notes instead of /users
+				.post('/notes')
+				// Note sent without a valid user should return an error
+				// We will use the user created above
+				.send( {
+					user_name: 'Test.Person',
+					body: 'This is a test note!'
+				})
+				.end(function() {
+					setTimeout(done, 200);
+				});
+		});
+		
+		/*
+		*** Now that we have everything set up, we need a test case
+		*** The acceptance criteria for the feature that a person be able to add new note
+		*** is that there be exactly ONE new message in the database with the user_name
+		*** we created at the beginning of the script.
+		*/
+
+		it('should find a new Note for user "Test.Person" ', function(done) {
+			Notes.find({user_name: 'Test.Person'}, function(err, results) {
+				assert.equal(1, results.length);
+				done();
+			});
+		});
 
 	});
 
