d328089e96372d13bf0227b598f247ab2bb81b68
Stephen Clark <stephenclark@MOBImage.local>
2014-06-15 21:51:20 -0400

    Updated test_template

--- test/test_template.js
+++ test/test_template.js
@@ -1,18 +1,19 @@
 var assert = require('assert');
 var dest = // Put the root URL for your requests
 
+//Don't be afraid to switch back to Master branch if you get stuck!
 describe('Notes Test', function() {
 
-/* 
+/*
 *** Create a user first, before the test is executed
-*** Your hint is to remember "hooks". 
+*** Your hint is to remember "hooks".
 */
 
 	before(function(done) {
 		// Insert Code here
 	});
 
-/* 
+/*
 *** In similar fashion to the "before" block above, this code will execute after EVERY test
 *** in this script's "Describe" block. We could opt to have these before/after hooks in each block
 *** but this is just to clear out the database once we're done with this test.
@@ -23,10 +24,10 @@ describe('Notes Test', function() {
 	});
 
 // Try writing this code with beforeEach and afterEach. These hooks are designed to execute after each of the describe blocks nested inside
-	
+
 	describe('Note Created', function() {
-		
-		/*  
+
+		/*
 		*** This before function is nested inside this specific 'describe'
 		*** Therefore, anything in this block will execute before the 'it'
 		*** Can think of the 'it' as being the actual test case
@@ -44,24 +45,27 @@ describe('Notes Test', function() {
 					setTimeout(done, 200);
 				});
 		});
-		
+
 		/*
 		*** Now that we have everything set up, we need a test case
 		*** The acceptance criteria for the feature that a person be able to add new note
 		*** Is that there be exactly ONE new message in the database with the user_name
 		*** We created at the beginning of the script.
+		*** This might be a bit tricky. You'll need to figure out the created user's id for the GET to work
 		*/
 
 		it('should find a new Note for user "Test.Person" ', function(done) {
-			Notes.find({ }, function(err, results) { //Insert query parameters between {}
-				assert.equal(); // Insert "assertion" between ()
-				// Insert callback here
-			});
+			dest
+				.get('/users/'+id+'/notes')
+				.send()
+				.end(function() {
+					// Assert what allows this test to pass!
+				})
 		});
 
 	});
-	
-// For this one, you're on your own! Use all of the tools you now have, and build this test case yourself! 
+
+// For this one, you're on your own! Use all of the tools you now have, and build this test case yourself!
 	describe('Update Note', function() {
 		// Good luck!
 
